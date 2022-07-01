f467ffb2beccfb676c9e2eb2dcf7ebf4fe97c6c1
SergeiBak <77221025+SergeiBak@users.noreply.github.com>
2022-06-30 22:44:27 -0700

    Scoring
    Scoring setup & synced with UI. High score saved

--- projectCode/Space Invaders/Assets/Scripts/GameManager.cs
+++ projectCode/Space Invaders/Assets/Scripts/GameManager.cs
@@ -24,6 +24,24 @@ public class GameManager : MonoBehaviour
     private Image extraLifeIcon2;
 
     [SerializeField]
+    private Image scoreOnesPlace;
+    [SerializeField]
+    private Image scoreTensPlace;
+    [SerializeField]
+    private Image scoreHundredsPlace;
+    [SerializeField]
+    private Image scoreThousandsPlace;
+
+    [SerializeField]
+    private Image highScoreOnesPlace;
+    [SerializeField]
+    private Image highScoreTensPlace;
+    [SerializeField]
+    private Image highScoreHundredsPlace;
+    [SerializeField]
+    private Image highScoreThousandsPlace;
+
+    [SerializeField]
     private Sprite zeroSprite;
     [SerializeField]
     private Sprite oneSprite;
@@ -50,6 +68,13 @@ public class GameManager : MonoBehaviour
         invaders = FindObjectOfType<Invaders>();
         mysteryShip = FindObjectOfType<MysteryShip>();
         bunkers = FindObjectsOfType<Bunker>();
+
+        if (!PlayerPrefs.HasKey("SpaceInvadersHighScore")) // if high score doesnt exist, initialize to zero
+        {
+            PlayerPrefs.SetInt("SpaceInvadersHighScore", 0);
+        }
+
+        UpdateHighScoreUI();
     }
 
     private void Start()
@@ -108,7 +133,74 @@ public class GameManager : MonoBehaviour
     private void SetScore(int score)
     {
         this.score = score;
-        //scoreText.text = score.ToString().PadLeft(4, '0');
+        UpdateScoreUI();
+
+        if (PlayerPrefs.GetInt("SpaceInvadersHighScore") < score) // if score greater than high score, high score becomes score
+        {
+            PlayerPrefs.SetInt("SpaceInvadersHighScore", score);
+            UpdateHighScoreUI();
+        }
+    }
+
+    private void UpdateScoreUI()
+    {
+        int scoreAmount = score;
+
+        scoreThousandsPlace.sprite = GetNumberSprite(scoreAmount / 1000);
+        scoreAmount = scoreAmount % 1000;
+
+        scoreHundredsPlace.sprite = GetNumberSprite(scoreAmount / 100);
+        scoreAmount = scoreAmount % 100;
+
+        scoreTensPlace.sprite = GetNumberSprite(scoreAmount / 10);
+        scoreAmount = scoreAmount % 10;
+
+        scoreOnesPlace.sprite = GetNumberSprite(scoreAmount);
+    }
+
+    private void UpdateHighScoreUI()
+    {
+        int scoreAmount = PlayerPrefs.GetInt("SpaceInvadersHighScore");
+
+        highScoreThousandsPlace.sprite = GetNumberSprite(scoreAmount / 1000);
+        scoreAmount = scoreAmount % 1000;
+
+        highScoreHundredsPlace.sprite = GetNumberSprite(scoreAmount / 100);
+        scoreAmount = scoreAmount % 100;
+
+        highScoreTensPlace.sprite = GetNumberSprite(scoreAmount / 10);
+        scoreAmount = scoreAmount % 10;
+
+        highScoreOnesPlace.sprite = GetNumberSprite(scoreAmount);
+    }
+
+    private Sprite GetNumberSprite(int digit)
+    {
+        switch (digit)
+        {
+            case 0:
+                return zeroSprite;
+            case 1:
+                return oneSprite;
+            case 2:
+                return twoSprite;
+            case 3:
+                return threeSprite;
+            case 4:
+                return fourSprite;
+            case 5:
+                return fiveSprite;
+            case 6:
+                return sixSprite;
+            case 7:
+                return sevenSprite;
+            case 8:
+                return eightSprite;
+            case 9:
+                return nineSprite;
+        }
+
+        return null;
     }
 
     private void SetLives(int lives)
